2867bf40da560de283907c03fff4e387fd9b52ea
Maksym Kudlaienko <kudlik0408@gmail.com>
2023-11-29 14:57:45 +0200

    Twitter-Analog: init commit

---
+++ src/main/groovy/com/direcsik/project/ProjectApplication.groovy
@@ -0,0 +1,13 @@
+package com.direcsik.project
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ProjectApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ProjectApplication, args)
+    }
+
+}
